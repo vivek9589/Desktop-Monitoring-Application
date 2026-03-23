4d0cbc226e6ff7c4564bb922e892035cc0ad668d
vivek9589 <vivekrathorevkr2001@gmail.com>
2026-03-23 16:38:21 +0530

    Refactored and Segregated application properties files into dev and prod along with the .env file

--- src/main/java/com/braininventory/monitoring/screenshot/monitor/agent/monitoring/entity/ApplicationUsage.java
+++
@@ -1,4 +0,0 @@
-package com.braininventory.monitoring.screenshot.monitor.agent.monitoring.entity;
-
-public class ApplicationUsage {
-}
--- src/main/java/com/braininventory/monitoring/screenshot/monitor/agent/monitoring/repository/ApplicationRepository.java
+++
@@ -1,4 +0,0 @@
-package com.braininventory.monitoring.screenshot.monitor.agent.monitoring.repository;
-
-public interface ApplicationRepository {
-}
